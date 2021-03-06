22816fb68c1d1c61fbdc029bae6db1cec7868304
yyz845935161 <51375977+yyz845935161@users.noreply.github.com>
2021-03-06 22:12:56 +0800

    Add files via upload

--- c++_test_3_6.cpp
+++ c++_test_3_6.cpp
@@ -40,3 +40,35 @@ int main()
 	return 0;
 		 
 }
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+
+	cout << (a + b - c) * d;
+	system("pause");
+	return 0;
+}
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int a;
+	cin >> a;
+
+	if (a % 5 == 0)cout << "YES";
+	else cout << "NO";
+
+	system("pause");
+	return 0;
+}
