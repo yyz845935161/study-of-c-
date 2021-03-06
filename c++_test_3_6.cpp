2095940152758c750722c26bb5953ba818d8a0b8
yyz845935161 <51375977+yyz845935161@users.noreply.github.com>
2021-03-06 14:52:37 +0800

    Add files via upload

---
+++ c++_test_3_6.cpp
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+using namespace std;
+
+class Man
+{
+public:
+	string M_name;
+	int M_age;
+	static string M_sex;
+
+	Man(string name, int age) :M_name(name), M_age(age)
+	{}
+	
+	void coutSafe()
+	{
+		cout << "安全性" << M_safe << endl;
+	}
+
+private:
+	static string M_safe;
+};
+
+string Man::M_sex = "nan";
+string Man::M_safe ="安全";
+
+void test1()
+{
+	Man A("张三", 18);
+	cout << "张三的性别" << A.M_sex<<endl;
+	A.coutSafe();
+}
+
+
+int main()
+{
+	test1();
+	system("pause");
+	return 0;
+		 
+}
