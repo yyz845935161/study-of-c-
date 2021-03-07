d839b42dcc6a40e72b00cec566c673e9e1437f9e
yyz845935161 <51375977+yyz845935161@users.noreply.github.com>
2021-03-07 21:28:58 +0800

    Add files via upload

--- c++_test_3_7.cpp
+++ c++_test_3_7.cpp
@@ -80,57 +80,139 @@ using namespace std;
 //	return 0;
 //}
 
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    int x, y;
+//    cin >> x;
+//    if (x > 0)cout << -1;
+//    else if (x == 0)cout << 0;
+//    else cout << 1;
+//
+//    system("pause");
+//    return 0;
+//}
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    int i = 2019;
+//    int count = 0;
+//    while (i)
+//    {
+//        if (i % 10 == 9 || i / 10 % 10 == 9 || i / 100 % 10 == 9)
+//            count++;
+//        i--;
+//    }
+//    cout << count;
+//    system("pause");
+//    return 0;
+//}
+//
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    int jnum = 0;
+//    int onum = 0;
+//    int i;
+//    cin >> i;
+//    while (i)
+//    {
+//        if (i % 2 == 0)onum++;
+//        else jnum++;
+//        i--;
+//    }
+//    cout << jnum << " " << onum;
+//    system("pause");
+//    return 0;
+//}
+//
+
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    int n = 0;
+//    int arr[50];
+//    cin >> n;
+//    for (int i = 0; i < n; i++)
+//    {
+//        cin >> arr[i];
+//    }
+//    cin >> arr[n];
+//    for (int i = 0; i < n + 1; i++)
+//    {
+//        for (int j = 0; j < n - i; j++)
+//        {
+//            if (arr[j] > arr[j + 1])
+//            {
+//                int temp = arr[j];
+//                arr[j] = arr[j + 1];
+//                arr[j + 1] = temp;
+//            }
+//
+//        }
+//    }
+//
+//    for (int i = 0; i <= n; i++)
+//    {
+//        cout << arr[i] << " ";
+//    }
+//
+//}
+
 
 #include <iostream>
+#include <string.h>
 using namespace std;
+
 
 int main()
 {
-    int x, y;
-    cin >> x;
-    if (x > 0)cout << -1;
-    else if (x == 0)cout << 0;
-    else cout << 1;
 
+    char  password[20];
+    char password_again[20];
+    cin >> password >> password_again;
+    if (strcmp(password, password_again) == 0)
+        cout << "same";
+    else cout << "different";
     system("pause");
     return 0;
 }
 
+
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
-    int i = 2019;
-    int count = 0;
-    while (i)
+    int n, m, sum = 0;
+    cin >> n >> m;
+    int arr[n][m];
+    for (int i = 0; i < n; i++)
     {
-        if (i % 10 == 9 || i / 10 % 10 == 9 || i / 100 % 10 == 9)
-            count++;
-        i--;
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+            if (arr[i][j] > 0)
+                sum += arr[i][j];
+        }
+
     }
-    cout << count;
+    cout << sum;
+
     system("pause");
     return 0;
 }
 
-
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    int jnum = 0;
-    int onum = 0;
-    int i;
-    cin >> i;
-    while (i)
-    {
-        if (i % 2 == 0)onum++;
-        else jnum++;
-        i--;
-    }
-    cout << jnum << " " << onum;
-    system("pause");
-    return 0;
-}
