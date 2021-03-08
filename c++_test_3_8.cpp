31a08b27fd6799af7f770e3061fa25cb37ca0689
yyz845935161 <51375977+yyz845935161@users.noreply.github.com>
2021-03-08 20:34:07 +0800

    Add files via upload

--- c++_test_3_8.cpp
+++ c++_test_3_8.cpp
@@ -35,11 +35,53 @@ using namespace std;
 //{
 //	son a;
 //}
+//
+//int main()
+//{
+//	//test();
+//
+//	system("pause");
+//	return 0;
+//}
 
+#include <iostream>
+using namespace std;
 int main()
 {
-	//test();
 
-	system("pause");
-	return 0;
+    int arr[50] = { 0 };
+    int temp[50] = { 0 };
+    int n;
+    int num = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int x = 1;
+        for (int j = 0; j < num; j++)
+        {
+            if (temp[j] == arr[i])
+            {
+                x = 0;
+            }
+        }
+
+        if (x == 1)
+        {
+            temp[num] = arr[i];
+            num++;
+        }
+
+    }
+
+    for (int i = 0; i < num; i++)
+    {
+        cout << temp[i] << " ";
+    }
+    system("pause");
+    return 0;
 }
