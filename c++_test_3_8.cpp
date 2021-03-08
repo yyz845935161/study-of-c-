41fed1529197e47192635886fd028dc332543152
yyz845935161 <51375977+yyz845935161@users.noreply.github.com>
2021-03-08 21:06:06 +0800

    Add files via upload

--- c++_test_3_8.cpp
+++ c++_test_3_8.cpp
@@ -85,3 +85,78 @@ int main()
     system("pause");
     return 0;
 }
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, m;
+    int arr1[10] = { 0 };
+    int arr2[10] = { 0 };
+    int temp1 = 0;
+    int temp2 = 0;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr1[i];
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cin >> arr2[i];
+    }
+
+    for (int i = 0; i < n + m; i++)
+    {
+        if (arr1[temp1] < arr2[temp2])
+        {
+            cout << arr1[temp1] << " ";
+            temp1++;
+            if (temp1 == n)
+            {
+                for (; i < n + m - 1; i++)
+                {
+                    cout << arr2[temp2++] << " ";
+                }
+                break;
+            }
+        }
+        else
+        {
+            cout << arr2[temp2] << " ";
+            temp2++;
+            if (temp2 == m)
+            {
+                for (; i < n + m - 1; i++)
+                {
+                    cout << arr1[temp1++] << " ";
+                }
+                break;
+            }
+        }
+    }
+    system("pause");
+    return 0;
+}
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, m;
+    float arr[5][6] = { 0 };
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cin >> arr[i][j];
+            arr[i][5] += arr[i][j];
+            cout << arr[i][j] << " ";
+
+        }
+        cout << arr[i][5] << endl;
+
+    }
+    system("pause");
+    return 0;
+}
