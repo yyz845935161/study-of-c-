b25e08a2cc37e791723061fb8e846ec98807931d
yyz845935161 <51375977+yyz845935161@users.noreply.github.com>
2021-03-08 16:28:23 +0800

    Add files via upload

--- c++_test_3_8.cpp
+++ c++_test_3_8.cpp
@@ -1,90 +1,44 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-
 #include <iostream>
 using namespace std;
-
-//int main()
+//class father
 //{
-//	int* p = new int;
-//	int p2;
-//	cout << p << endl;
-//	cout <<&p2;
-//	system("pause");
-//	return 0;
+//public:
+//	father()
+//	{
+//		cout << "这是father的构造函数" << endl;
+//	}
+//	~father()
+//	{
+//		cout << "这是father的析构函数" << endl;
+//	}
+//
+//};
+//
+//
+//class son: public father
+//{
+//public:
+//	son()
+//	{
+//		cout << "这是son的构造函数" << endl;
+//	}
+//	~son()
+//	{
+//		cout << "这是son的析构函数" << endl;
+//	}
+//
+//};
+//
+//
+//void test()
+//{
+//	son a;
 //}
 
-class MyInteger
-{
-public:
-	MyInteger()
-	{
-		M_num = 0;
-	}
-	int M_num;
-
-	MyInteger& operator--()
-	{
-		M_num--;
-		return *this;
-	}
-
-	 MyInteger operator--(int)
-	{
-		 MyInteger temp = *this;
-		 M_num--;
-		 return temp;
-
-	}
-
-
-
-};
-
-ostream& operator<<(ostream& out,MyInteger p)
-{
-	out << "p.M_num="<<p.M_num;
-	return out;
-}
-
-void test0()
-{
-	int a = 0;
-	cout << a << endl;
-	cout << (a--) << endl;
-	cout <<(a--)<< endl;
-
-}
-
-void test01()
-{
-	MyInteger a;
-	cout << a << endl;
-	cout << (a--) << endl;
-	cout << (a--)<< endl;
-}
-
-void test02()
-{
-	MyInteger a;
-	cout << a << endl;
-	cout << --a << endl;
-	cout << --(--a) << endl;
-}
-
-void test03()
-{
-	int i = 0;
-	cout << i << endl;
-	cout << --i << endl;
-	cout << --(--i) << endl;
-
-}
 int main()
 {
-	test0();
-	test01();
-	test02();
-	test03();
+	//test();
 
 	system("pause");
 	return 0;
