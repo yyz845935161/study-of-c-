2b53f90801f2a092c151aba0511a3d88d1bb7a04
yyz845935161 <51375977+yyz845935161@users.noreply.github.com>
2021-03-09 22:38:01 +0800

    Add files via upload

--- c++_test_3_9.cpp
+++ c++_test_3_9.cpp
@@ -35,3 +35,32 @@ int main()
     system("pause");
     return 0;
 }
+
+
+#include<stdio.h>
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+    int arr[30][30];
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j <= i; j++)
+        {
+            if (j == 0 || j == i)
+            {
+                arr[i][j] = 1;
+                printf("%5d", 1);
+            }
+            else
+            {
+                arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+                printf("%5d", arr[i][j]);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
