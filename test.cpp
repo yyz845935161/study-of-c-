d5e7deaf57b2fb3a5f5358981bdca28cac6383ab
yyz845935161 <51375977+yyz845935161@users.noreply.github.com>
2021-03-13 22:00:16 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,70 +1,61 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+#include "SeqList.h"
 
-#include<iostream>
-#include<string>
-#include<ctime>
-using namespace std;
-//学生的结构体定义
-struct Student
-{
-	//姓名
-	string sName;
-	//分数
-	int score;
-};
-//老师的结构体定义
-struct Teacher
-{
-	//姓名
-	string tName;
-	//学生数组
-	struct Student sArray[5];
-};
-void allocateSpace(struct Teacher tArray[], int len)
-{
-	int i = 0;
-	string nameSeed = "ABCDEF";
-	for (i = 0; i < len; i++)
-	{
-		tArray[i].tName = "Teacher_";
-		tArray[i].tName += nameSeed[i];
-		//通过循环给每个老师所带的学生赋初值
-		int j = 0;
 
-		for (j = 0; j < len; j++)
-		{
-			tArray[i].sArray[j].sName = "Student";
-			tArray[i].sArray[j].sName += nameSeed;
-			int random = rand() % 61 + 40;
-			tArray[i].sArray[j].score = random;
-		}
-	}
+//测试头尾插入
+void testSL()
+{
+	SL s; 
+	SeqListInit(&s);
+	SeqListPushBack(&s, 1);
+	SeqListPushBack(&s, 2);
+	SeqListPushBack(&s, 8);
+	SeqListPrint(&s);
+
+	SeqListPushFront(&s, 100);
+	SeqListPrint(&s);
+
 }
-void PrintInfo(struct Teacher tArray[], int len)
-{
-	int i = 0;
-	for (i = 0; i < len; i++)
-	{
-		cout << "老师姓名：" << tArray[i].tName << endl;
-		int j = 0;
-		for (j = 0; j < len; j++)
-		{
 
-			cout << "\t学生姓名： " << tArray[i].sArray[j].sName <<
-				"考试分数： " << tArray[i].sArray[j].score << endl;
-		}
-	}
+//测试头插入
+void testHeadpush()
+{
+	SL s;
+	SeqListInit(&s);
+	SeqListPushFront(&s, 100);
+	SeqListPushFront(&s, 1);
+}
+
+void testHeadPop()
+{
+	SL s;
+	SeqListInit(&s);
+	SeqListPushBack(&s, 1);
+	SeqListPushBack(&s, 2);
+	SeqListPushBack(&s, 8);
+	SeqListPopFront(&s);
+
+
+	SeqListPushFront(&s, 100);
+	SeqListPushFront(&s, 11);
+	SeqListPushFront(&s, 3);
+	SeqListPopFront(&s); 
+	SeqListPushFront(&s, 66);
+
+	SeqListInsert(&s,1,2);
+	SeqListInsert(&s, 1,3);
+	SeqListInsert(&s, 1,4);
+	
+
+	
+
+
+	
 }
 int main()
 {
-	//随机数种子
-	srand((unsigned int)time(NULL));
-	//创建三名老师的数组
-	struct Teacher tArray[3];
-	//通过函数给三名老师的信息赋值，并且给老师带的学生赋值
-	int len = sizeof(tArray) / sizeof(tArray[0]);
-	allocateSpace(tArray, len);
-	//打印所有老师及所带老师的信息 
-	PrintInfo(tArray, len);
+	testHeadPop();
+	//testSL();
+	//testHeadpush();
+
 	return 0;
 }
