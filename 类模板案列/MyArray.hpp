66eeff82baace367d7810c216dfa2bb78955282f
yyz845935161 <51375977+yyz845935161@users.noreply.github.com>
2021-03-16 16:56:16 +0800

    Add files via upload

--- 类模板案列/MyArray.hpp
+++ 类模板案列/MyArray.hpp
@@ -17,19 +17,72 @@ public:
 	//构造函数
 	MyArray(int n)
 	{
-		pAddress = new T[n];
+		cout << "调用了有参构造" << endl;
+		this->m_capacity = n;
+		this->m_size = 0;
+		this->pAddress = new T[this->m_capacity];
 	}
 
 	//拷贝函数
-	MyArray(const Myarry& arr)
+	MyArray(const MyArray& arr)
 	{
-		m_capacity = arr.m_capacity;
-		m_size = arr.m_size;
-		pAddress = new T[arr.m_capacity];
+		cout << "调用了拷贝构造" << endl;
+		this->m_capacity = arr.m_capacity;
+		this->m_size = arr.m_size;
+
+		//深拷贝
+		this->pAddress = new T[arr.m_capacity];
+
+		//将arr中的数据都拷贝过来
+		for (int i = 0; i < arr.m_size;i++)
+		{
+			this->pAddress[i] = arr.pAddress[i];
+		}
 	}
 
+
 	//赋值运算符重载
-	MyArray& opearate= (const MyArray& arr);
+	//返回自身引用是为了连等
+	MyArray& operator = (const MyArray & arr)
+	{
+		cout << "调用operator=构造" << endl;
+		if (this->pAddress!=NULL)
+		{
+			delete[] this->pAddress;
+			this->pAddress = NULL;
+			this->m_capacity = 0;
+			this->m_size = 0;
+		}
+
+		this->m_capacity = arr.m_capacity;
+		this->m_size = arr.m_size;
+
+		//深拷贝
+		this->pAddress = new T[arr.m_capacity];
+
+		//将arr中的数据都拷贝过来
+		for (int i = 0; i < arr.m_size; i++)
+		{
+			this->pAddress[i] = arr.pAddress[i];
+		}
+
+	}
+
+
+	//析构函数
+	~MyArray()
+	{
+		cout << "析构调用" << endl;
+		if (this->pAddress!=NULL)
+		{
+			delete[] this->pAddress;
+			this->pAddress = NULL;
+		}
+	}
+
+	
+
+	
 
 	
 };
---
+++ 类模板案列/类案例模板.cpp
@@ -0,0 +1,11 @@
+#include"MyArray.hpp"
+
+void test01()
+{
+	MyArray<int>arr1(5);
+}
+
+int main()
+{
+	return 0;
+}
