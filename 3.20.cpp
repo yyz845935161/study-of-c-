a1bd822a932621b40bf822330702011767a75959
yyz845935161 <51375977+yyz845935161@users.noreply.github.com>
2021-03-20 22:40:27 +0800

    Add files via upload

--- 3.20.cpp
+++ 3.20.cpp
@@ -25,63 +25,112 @@
 //	return 0;
 //}
 
+//
+//int removeDuplicates(int* nums, int numsSize) {
+//    int mark = 0;
+//    for (int i = 1; i < numsSize - mark; i++)
+//    {
+//        if (nums[i] == nums[i - 1])
+//        {
+//            for (int j = i--; j < numsSize - 1 - mark; j++)
+//            {
+//                nums[j] = nums[j + 1];
+//            }
+//            mark++;
+//
+//        }
+//    }
+//
+//    numsSize -= mark;
+//    return numsSize;
+//}
+//
+//int main()
+//{
+//    int arr[10] = {0,0,1,1,1,2,2,3,3,4};
+//    removeDuplicates(arr, 10);
+//    return 0;
+//}
+//
+//
+//void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n)
+//{
+//    int n1 = 0;
+//    int n2 = 0;
+//    for (int i = 0; i < m + n; i++)
+//    {
+//        if (n2 == n)break;
+//        if (n1 == m)
+//        {
+//            for (int j = nums1Size; j < m + n; j++)
+//            {
+//                nums1[j] = nums2[n2++];
+//                nums1Size++;
+//            }
+//            break;
+//        }
+//
+//        if (nums1[n1] > nums2[n2])
+//        {
+//            for (int j = nums1Size - 1; j > n1; n--)
+//            {
+//                nums1[j] = nums1[j - 1];
+//            }
+//            nums1[n1] = nums2[n2];
+//            nums1Size++;
+//            n2++;
+//        }
+//        n1++;
+//    }
+//}
 
-int removeDuplicates(int* nums, int numsSize) {
-    int mark = 0;
-    for (int i = 1; i < numsSize - mark; i++)
-    {
-        if (nums[i] == nums[i - 1])
-        {
-            for (int j = i--; j < numsSize - 1 - mark; j++)
-            {
-                nums[j] = nums[j + 1];
-            }
-            mark++;
+//
+//int i = m-- + --n;
+//
+//while (n >= 0) {
+//    nums1[i--] = m >= 0 && nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
 
-        }
-    }
 
-    numsSize -= mark;
-    return numsSize;
-}
-
-int main()
+void ro(int* nums, int a, int b)
 {
-    int arr[10] = {0,0,1,1,1,2,2,3,3,4};
-    removeDuplicates(arr, 10);
-    return 0;
-}
-
-
-void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n)
-{
-    int n1 = 0;
-    int n2 = 0;
-    for (int i = 0; i < m + n; i++)
+    while (a < b)
     {
-        if (n2 == n)break;
-        if (n1 == m)
-        {
-            for (int j = nums1Size; j < m + n; j++)
-            {
-                nums1[j] = nums2[n2++];
-                nums1Size++;
-            }
-            break;
-        }
-
-        if (nums1[n1] > nums2[n2])
-        {
-            for (int j = nums1Size - 1; j > n1; n--)
-            {
-                nums1[j] = nums1[j - 1];
-            }
-            nums1[n1] = nums2[n2];
-            nums1Size++;
-            n2++;
-        }
-        n1++;
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+        a++;
+        b--;
     }
 }
 
+void rotate(int* nums, int numsSize, int k) {
 
+    if (k == 0)return;
+    ro(nums, 0, numsSize - 1 - k);
+    ro(nums, numsSize - k, numsSize - 1);
+    ro(nums, 0, numsSize - 1);
+
+}
+
+
+void ro(int* nums, int a, int b)
+{
+    while (a < b)
+    {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+        a++;
+        b--;
+    }
+}
+
+void rotate(int* nums, int numsSize, int k) {
+
+    k %= numsSize;
+
+    ro(nums, 0, numsSize - 1 - k);
+    ro(nums, numsSize - k, numsSize - 1);
+    ro(nums, 0, numsSize - 1);
+
+}
