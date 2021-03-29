edca0a7c9ad64e3bc95bd5c61144f49173152949
yyz845935161 <51375977+yyz845935161@users.noreply.github.com>
2021-03-29 11:20:04 +0800

    Add files via upload

--- 3.29.cpp
+++ 3.29.cpp
@@ -1,6 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <iostream>
 #include <list>
+
+#include<vector>
+#include<deque>
+#include<set>
 using namespace std;
 
 class Person
@@ -26,6 +30,14 @@ void myprint(const list<Person>& L)
 	}
 }
 
+void myprint(const set<Person>& L)
+{
+	for (set<Person>::const_iterator it = L.begin(); it != L.end(); it++)
+	{
+		cout << "身高： " << it->name << "  年龄： " << it->age << "  身高： " << it->hight << endl;
+	}
+}
+
 
 //指定排序规则
 bool comparePerson(Person& p1,Person& p2)
@@ -45,6 +57,7 @@ void test01()
 	Person p5("张飞", 35, 160);
 	Person p6("关羽", 35, 200);
 
+
 	L.push_back(p1);
 	L.push_back(p2);
 	L.push_back(p3);
@@ -60,8 +73,47 @@ void test01()
 
 }
 
+void test02()
+{
+	vector<int>v;
+	v.push_back(10);
+	v.push_back(10);
+	v.push_back(160);
+	v.push_back(70);
+	v.push_back(20);
+	v.push_back(10);
+	v.insert(v.begin(), 10);
+	
+
+	//v.count();//没有
+}
+
+
+void test03()
+{
+	deque<int >d;
+	d.push_back(10);
+	
+}
+
+void test04()
+{
+	set<Person>s;
+	//数据
+	Person p1("刘备", 35, 175);
+	Person p2("曹操", 45, 180);
+	Person p3("孙权", 40, 170);
+	Person p4("赵云", 25, 190);
+	Person p5("张飞", 35, 160);
+	Person p6("关羽", 35, 200);
+
+	s.insert(p1);
+	myprint(s);
+}
+
 int main()
 {
-	test01();
+	//test01();
+	test04();
 	return 0;
 }
