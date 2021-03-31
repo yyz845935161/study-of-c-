db497994c04656fc8733b7f40ab36e70acabf12a
yyz845935161 <51375977+yyz845935161@users.noreply.github.com>
2021-03-31 16:00:36 +0800

    Add files via upload

--- 3.31.cpp
+++ 3.31.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 using namespace std;
 #include<algorithm>
+#include<functional>
 
 class GreatFive
 {
@@ -33,9 +34,91 @@ void test01()
 	}
 }
 
+class mycompare
+{
+public:
+	mycompare()
+	{
+		cout << "无参构造" << endl;
+	}
+	bool operator()(int a,int b)
+	{
+		cout << "调用了此函数" << endl;
+		return a > b;
+	}
+};
+
+
+void myPrint(vector<int>& v)
+{
+	for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+}
+
+void test02()
+{
+	vector<int>v;
+	v.push_back(10);
+	v.push_back(30);
+	v.push_back(20);
+	v.push_back(40);
+
+	myPrint(v);
+
+	//降序排序
+	sort(v.begin(), v.end(),mycompare());
+
+	myPrint(v);
+
+
+	//cout << endl;
+	//mycompare a;
+	//cout << a(3, 5) << endl;
+
+	//mycompare();
+
+
+	cout << endl;
+}
+
+void test03()
+{
+	vector<bool>v;
+	v.push_back(true);
+	v.push_back(true);
+	v.push_back(true);
+	v.push_back(false);
+
+	for (vector<bool>::iterator it = v.begin(); it !=v.end(); it++)
+	{
+		cout << *it << "  ";
+	}
+	cout << endl;
+
+
+	vector<bool>v2;
+
+	v2.resize(v.size());
+
+	transform(v.begin(), v.end(), v2.begin(), logical_not<bool>());
+
+	for (vector<bool>::iterator it = v2.begin(); it != v2.end(); it++)
+	{
+		cout << *it << "  ";
+	}
+	cout << endl;
+}
+
 
 int main()
 {
-	test01();
+	//mycompare();
+	//test02();
+	//test01();
+
+	test03();
 	return 0;
 }
