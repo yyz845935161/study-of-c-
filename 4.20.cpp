6ec73b94047dfe9402aad0426317d99a94631222
yyz845935161 <51375977+yyz845935161@users.noreply.github.com>
2021-04-20 22:14:20 +0800

    Add files via upload

---
+++ 4.20.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<ctime>
+//
+//int main()
+//{
+//	int money = 0;
+//	int i = 100;
+//	while(i--)
+//	{
+//		printf("给TT充钱10000元成功，当前余额%d\n", money);
+//		money += 10000;
+//	}
+//	return 0;
+//}
+
